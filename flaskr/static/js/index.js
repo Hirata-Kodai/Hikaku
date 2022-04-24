d2982a38e974441ffbd0820af644be6e291064e0
migichan3ist <migichan@ist.hokudai.ac.jp>
2022-04-24 16:29:50 +0900

    フロントエンドある程度完成

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -10,43 +10,125 @@ document.getElementById("submit-button").onclick = function() {
     var noodleName =  $('#noodle_name').val();
     var noodle = $('#noodle').val();
     var soupKoku =  $('#soup-koku').val();
+    var ingredientsNum =  $('#ingredients_num').val();
     var price = $('#price').val();
     noodleFrameNum += 1;
     
     var appendTag = "<li id='noodle-frame-" +  noodleFrameNum + "'> \
-                        <div class='submit-area' > \
-                            <div class='submit-area-img' > \
-                                <img id='uploaded-image-" +  noodleFrameNum + "' class='submitted_img'><br>\
-                            </div>\
-                            <div class='submit-area-compared_things' > \
+                        <div class='submit-area mx-8 my-6 flex font-sans rounded-md bg-white relative'> \
+                            <div class='flex-none w-64 relative'> \
+                                <div class='h-3/5'> \
+                                    <img id='uploaded-image-" +  noodleFrameNum + "' class='p-6 inset-0 w-full h-full object-cover'><br>\
+                                </div> \
+                            </div> \
+                            <div class='submit-area-compared_things flex-auto p-2' > \
                                 <ul class='detail-things'> \
                                     <li class='noodle-shop-name-frame'></li> \
-                                        お店の名前：<input type='text' id='noodle_shop_name' name='noodle_shop_name' value='" + noodleShopName + "'> \
+                                        <div class='flex my-3'> \
+                                            <div class='w-1/3 mt-1 px-3 py-2'> \
+                                                お店の名前： \
+                                            </div> \
+                                            <div class='w-2/3'> \
+                                                <input type='text' id='noodle_shop_name' name='noodle_shop_name' value='" + noodleShopName + "' class='w-2/3 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1'> \
+                                            </div> \
+                                        </div> \
                                     </li> \
                                     <li class='noodle-name-frame'></li> \
-                                        ラーメンの名前：<input type='text' id='noodle_name' name='noodle_name' value=' " + noodleName +" '> \
+                                        <div class='flex my-3'> \
+                                            <div class='w-1/3 mt-1 px-3 py-2'> \
+                                                ラーメンの名前： \
+                                            </div> \
+                                            <div class='w-2/3'> \
+                                                <input type='text' id='noodle_name' name='noodle_name' value=' " + noodleName +" ' class='w-2/3 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1'> \
+                                            </div> \
+                                        </div> \
                                     </li> \
                                     <li class='compared_thing_range'> \
-                                        <input type='range' id='noodle' name='noodle' class='range-class'\
+                                        <div class='flex my-3'> \
+                                            <div class='w-2/5 mt-1 px-3 flex'> \
+                                                <div class='w-3/5'> \
+                                                <label for='noodle'>麺のうまさ</label> \
+                                                </div> \
+                                                <div class='w-2/5'> \
+                                                <input type='text' id='noodle_num' name='noodle_num' value='" + noodle + "' class='w-4/5 view-range-value-class'> \
+                                                </div> \
+                                            </div> \
+                                            <div class='w-3/5 mt-1 px-3'> \
+                                                <div class='h-2/3'> \
+                                                    <input type='range' id='noodle' name='noodle' class='range-class w-4/5 h-2 bg-blue-100 appearance-none' \
                                             min='0' max='100' value='" + noodle + "'> \
-                                        <label for='noodle'>麺のうまさ</label> \
-                                        <input type='text' id='noodle_num' name='noodle_num' value='" + noodle + "' class='view-range-value-class'> \
+                                                    </div> \
+                                                <div lass='h-1/3'> \
+                                                    <div class='flex w-4/5 justify-between'> \
+                                                        <span>0</span><span>100</span> \
+                                                    </div> \
+                                                </div> \
+                                            </div> \
+                                        </div> \
                                     </li> \
                                     <li class='compared_thing_range'> \
-                                        <input type='range' id='soup-koku' name='soup-koku' class='range-class'\
+                                        <div class='flex my-3'> \
+                                            <div class='w-2/5 mt-1 px-3 flex'> \
+                                                <div class='w-3/5'> \
+                                                <label for='soup-koku'>スープのうまさ</label> \
+                                                </div> \
+                                                <div class='w-2/5'> \
+                                                <input type='text' id='noodle_num' name='noodle_num' value='" + soupKoku + "' class='w-4/5 view-range-value-class'> \
+                                                </div> \
+                                            </div> \
+                                            <div class='w-3/5 mt-1 px-3'> \
+                                                <div class='h-2/3'> \
+                                                    <input type='range' id='soup-koku' name='soup-koku' class='range-class w-4/5 h-2 bg-blue-100 appearance-none' \
                                             min='0' max='100' value='" + soupKoku + "'> \
-                                        <label for='soup-koku'>スープのうまさ</label> \
-                                        <input type='text' id='noodle_num' name='noodle_num' value='" + soupKoku + "' class='view-range-value-class'> \
+                                                </div> \
+                                                <div lass='h-1/3'> \
+                                                    <div class='flex w-4/5 justify-between'> \
+                                                        <span>0</span><span>100</span> \
+                                                    </div> \
+                                                </div> \
+                                            </div> \
+                                        </div> \
+                                    </li> \
+                                    <li class='compared_thing_range'> \
+                                        <div class='flex my-3'> \
+                                            <div class='w-2/5 mt-1 px-3 flex'> \
+                                                <div class='w-3/5'> \
+                                                    <label for='ingredients'>具材のうまさ</label> \
+                                                </div> \
+                                                <div class='w-2/5'> \
+                                                    <input type='text' id='ingredients_num' name='ingredients_num' value='" + ingredientsNum + "'class='w-4/5 view-range-value-class'> \
+                                                </div> \
+                                            </div> \
+                                            <div class='w-3/5 mt-1 px-3'> \
+                                                <div class='h-2/3'> \
+                                                    <input type='range' id='ingredients' name='ingredients' class='range-class w-4/5 h-2 bg-blue-100 appearance-none' \
+                                            min='0' max='100' ingredientsNum> \
+                                                </div> \
+                                                <div lass='h-1/3'> \
+                                                    <div class='flex w-4/5 justify-between'> \
+                                                        <span>0</span><span>100</span> \
+                                                    </div> \
+                                                </div> \
+                                            </div> \
+                                        </div> \
                                     </li> \
                                     <li class='compared_thing_text'> \
-                                        価格：<input type='number' id='price' name='price' min='0' max='10000' value='" + price + "'> \
-                                        <label for='price'>円</label> \
+                                        <div class='flex my-3'> \
+                                            <div class='w-1/3 mt-1 px-3 py-2'> \
+                                                価格： \
+                                            </div> \
+                                            <div class='w-1/3'> \
+                                                <input type='number' id='price' name='price' min='0' max='10000' value='" + price + "' class='mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1'> \
+                                            </div> \
+                                            <div class='w-1/3 mt-1 px-3 py-2'> \
+                                                <label for='price'>円</label> \
+                                            </div> \
+                                        </div> \
                                     </li> \
                                 </ul> \
-                                <input id='delete-button' class='delete-button-" +  noodleFrameNum + "' type='button' name='submit' value='削除' onclick='deleteClick(" + noodleFrameNum + ")'/> \
                             </div> \
                         </div> \
-                    </li>" 
+                    </li>"  
     
     $("#element-list").append(appendTag);
 
